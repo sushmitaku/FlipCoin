00b3cacdf0f249edd3a7c018dd8ac3f6df903a84
sushmitaku <ksushmita1999@gmail.com>
2020-09-10 13:22:47 +0530

    Number of times Head and Tail won is Declared

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,20 @@
 Winner=$(( RANDOM%2))
 Head=1
 Tail=0
-if [ $Head -eq $Winner ]
-then
-	echo "Head is Winner"
-else
-	echo "Tail is Winner"
-fi
+HeadCount=0
+TailCount=0
+read -p "Enter the limit " limit
+for (( i=0; i<$limit; i++ ))
+do
+	Winner=$(( RANDOM%2))
+	if [ $Head -eq $Winner ]
+	then
+		(( HeadCount++ ))
+	else
+		(( TailCount++ ))
+	fi
+done
+
+echo  "No of times Head won" $HeadCount
+echo  "No of times Tail won" $TailCount
 
