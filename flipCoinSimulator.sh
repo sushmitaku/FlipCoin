fa860be935990a793169e4669e8ecffab5658461
sushmitaku <ksushmita1999@gmail.com>
2020-09-10 14:14:06 +0530

    Who won 21 times First or Tie Between them is Declared

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,20 +1,44 @@
-Winner=$(( RANDOM%2))
-Head=1
-Tail=0
+read -p " Enter the Winning Limit" limit
 HeadCount=0
 TailCount=0
-read -p "Enter the limit " limit
-for (( i=0; i<$limit; i++ ))
+valid=true
+Head=1
+Tail=0
+while [ $valid ]
 do
-	Winner=$(( RANDOM%2))
-	if [ $Head -eq $Winner ]
-	then
-		(( HeadCount++ ))
-	else
-		(( TailCount++ ))
-	fi
+        Winner=$(( RANDOM %2 ))
+        if [ $Head -eq $Winner ]
+        then
+                (( ++HeadCount ))
+                if [ $HeadCount -eq $limit ]
+                then
+			Headdiff=$(($HeadCount-$TailCount ))
+			 if [ $Headdiff -eq 0 ]
+                        then
+                                echo "Tie between them"
+                                break
+                        fi
+
+                        echo "Head win first by $Headdiff Times"
+                        break
+                fi
+        fi
+        if [ $Tail -eq $Winner ]
+        then
+                (( ++TailCount ))
+                if [ $TailCount -eq $limit  ]
+                then
+			TailDiff=$(( $TailCount-$HeadCount ))
+			 if [ $TailDiff -eq 0 ]
+                        then
+                                echo "Tie Between them"
+                                        break
+                        fi
+
+                        echo "Tail win first by $TailDiff Times"
+                        break
+                fi
+        fi
 done
 
-echo  "No of times Head won" $HeadCount
-echo  "No of times Tail won" $TailCount
 
